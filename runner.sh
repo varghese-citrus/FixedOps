a85cb1d921963726fb55ee33825a46b88583563a
varghese-citrus <varghese.paily@citrusinformatics.com>
2023-02-15 18:59:48 +0530

    update project

--- runner.sh
+++ runner.sh
@@ -1,4 +1,5 @@
-cd /fixedops/data-validation
+#!/bin/bash
+cd /fixedops/data-validation/
 export FOPC_SITE_ID="suntrup2022" ROLE="admin" STORE="Suntrup Ford Westport" LOG="enable"
 deno test -A --unstable home-kpi-report-data-validation.ts
 
