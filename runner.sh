cffc265c9c952e72a4dc77d936547e009a30cab0
varghese-citrus <varghese.paily@citrusinformatics.com>
2023-02-15 19:23:58 +0530

    update runner file

--- runner.sh
+++ runner.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd fixedops/data-validation
-set -e
-export PATH="/root/.asdf/shims/deno"
 export FOPC_SITE_ID="suntrup2022" ROLE="admin" STORE="Suntrup Ford Westport" LOG="enable"
 deno test -A --unstable home-kpi-report-data-validation.ts
 
