ff8d6dc0a2af297ef4c67aa87b16a28c5536ad09
varghese-citrus <varghese.paily@citrusinformatics.com>
2023-02-16 16:00:53 +0530

    update data and ui runner file

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd fixedops/data-validation
 export FOPC_SITE_ID="suntrup2022" ROLE="admin" STORE="Suntrup Ford Westport" LOG="disable"
-deno test -A --unstable home-kpi-report-data-validation.ts
+deno test -A --unstable --jobs=5 --filter "AEC-FOCP-UI-FN-LD" *.ts
 
 
--- ui.sh
+++ ui.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd fixedops/testcases
 export FOPC_SITE_ID="suntrup2022" ROLE="admin" STORE="Suntrup Ford Westport" LOG="disable"
-deno test -A --unstable --parallel --filter "AEC-FOCP-UI-FN-LD" *.ts
+deno test -A --unstable --jobs=5 --filter "AEC-FOCP-UI-FN-LD" *.ts
